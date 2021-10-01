fb9cd52cfbda835907ffde6457a42589ce9b0efc
Kevin Khachi <coderkevin500@gmail.com>
2021-10-01 14:26:50 -0700

    "Improved fetch call image Quality"

--- src/components/Background.js
+++ src/components/Background.js
@@ -23,16 +23,24 @@ const Background = () => {
 
     try {
     const client = createClient('563492ad6f9170000100000139d0d4eff1a9443e9798b3bbaed0e386');
-    const query = 'Los Angeles';
-    const randomPic = Math.floor(Math.random() * 71);
+    let query = 'Los Angeles';
+    let randomPic = Math.floor(Math.random() * 71);
     const data = await client.photos.search({query, per_page: 70});
     console.log('Photo Data: ', data);
-    const url = data.photos[randomPic].src.large;
+    let picWidth = data.photos[randomPic].width;
+    let picHeight = data.photos[randomPic].height;
+    let url = data.photos[randomPic].src.original;
+    while (!((picWidth >= 2048 && picWidth <= 4096) && (picHeight <= 2160 && picHeight >= 1080))) {
+      randomPic = Math.floor(Math.random() * 71);
+      picWidth = data.photos[randomPic].width;
+      picHeight = data.photos[randomPic].height;
+    }
+    url = data.photos[randomPic].src.original;
     console.log('Photo URL: ', url);
     return url;
     } catch (error) {
       console.log(error + ' - Displaying Default Background');
-      return 'https://jooinn.com/images/german-mountains-2.jpg';
+      return 'https://source.unsplash.com/4096x2160/?nature,water';
     }
 
   }
