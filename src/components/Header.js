6ed5084684a76de54b5f0fcd3bc4eea6cea5faf5
Kevin Khachi <coderkevin500@gmail.com>
2021-10-08 04:09:31 -0700

    "Added a cog Icon and a currency text tab"

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import hamburger from '../images/icons/Hamburger.jpeg';
+import cog from '../images/icons/cog.png';
 
 const Header = () => {
 
@@ -9,7 +10,11 @@ const Header = () => {
       <span style={{verticalAlign: 'middle'}}>
       Weather App
       </span>
+      <div id='currency-title' style={{ fontSize: 25 + 'px', marginLeft : 90 + '%', position: 'absolute', bottom: 10}}>
+      Currency
+      </div>
       </h1>
+      <img id='hamburger' style={{maxWidth: 4.6 + '%', marginRight: 5 + '%', marginLeft: 93 + '%', marginTop: 5 + 'px'}} alt='' src={cog} />
     </div>
   )
 
