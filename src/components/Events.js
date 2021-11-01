1445466d148d474841d0fee8f8ae3873a7f33f58
Kevin Khachi <coderkevin500@gmail.com>
2021-10-31 20:02:42 -0700

    "Testing if merge updates previous commits on master"

--- src/components/Events.js
+++ src/components/Events.js
@@ -35,7 +35,6 @@ const Events = ({eventQuery}) => {
   return (
       <div id='event-tab'>
         <h1 id='event-title'>
-          Events
         </h1>
         <div className='events-type'>
           <h1 className='hover'>
